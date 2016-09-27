f9636ae3766e1b93b84c5d03f18cbb0c15c230db
aca535 <aca535@DRACO.INFORMATIK>
2016-09-27 14:32:29 +0200

    Beginn Parser

--- src/aufgabe01/Parser.java
+++ src/aufgabe01/Parser.java
@@ -1,5 +1,36 @@
 package aufgabe01;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
+	
+	private void Parser() {}
+	
+	private static Path dateiPfad;
+	private static List<String> geleseneZeilen;
+	
+	static void einlesenGraph() throws IOException {
+		dateiPfad = MeinFileChooser.chooseFile().toPath();
+		System.out.println(dateiPfad);
+		geleseneZeilen = Files.lines(dateiPfad).collect(Collectors.toList());
+		pruefenSyntax();
+		erstellenGrapen();
+		
+	}
+
+	private static void erstellenGrapen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void pruefenSyntax() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
---
+++ src/aufgabe01/TestKlasse.java
@@ -0,0 +1,10 @@
+package aufgabe01;
+
+import java.io.IOException;
+
+public class TestKlasse {
+
+	public static void main(String[] args) throws IOException {
+		Parser.einlesenGraph();
+	}
+}
