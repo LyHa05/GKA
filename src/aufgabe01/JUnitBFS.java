1381feaf4a5ca1acdd49a7816ad0398a8e4488fd
lpflug <lpflug@NTCG57.ntcg.local>
2016-10-12 12:00:12 +0200

    Pruefen Einfuegen eines Knotens!

---
+++ src/aufgabe01/JUnitBFS.java
@@ -0,0 +1,25 @@
+package aufgabe01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Lydia Pflug, Lucas Anders
+ * @date 12.10.2016
+ * Diese Klasse testet den BFS-Algorithmus.
+ */
+
+public class JUnitBFS {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
